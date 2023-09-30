c77b1c036c4a235fb598035037a1ddaece060b13
sandyysameh <sandy.sameh@prepitapp.com>
2023-09-30 17:24:09 +0300

    remove type

--- services/redux/types/SlicesTypes.tsx
+++
@@ -1,9 +0,0 @@
-import { FooterProps, HeaderProps } from "../../../utils/types";
-
-export interface LayoutState {
-  pageContentMargin: number;
-  layoutProps: {
-    headerProps: HeaderProps;
-    footerProps: FooterProps;
-  };
-}
